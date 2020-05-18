9df0d745842ebeff0b74a256a80035e0cabb640d
che.lin <aa10327tw@gmail.com>
2020-05-18 18:10:18 +0800

    Update 2020-05-18T10:09:33.031Z

--- app/js/week5/admin.js
+++
@@ -1,8 +0,0 @@
-// 側邊欄加入 active
-document.querySelector('a[data-sidebar="admin"]').classList.add('active');
-
-// header 加入標題
-document.querySelector('.header-title').innerText = 'Admin';
-
-// 隱藏課程下拉選單
-document.querySelector('.header-course-dropdown').classList.add('d-none');
