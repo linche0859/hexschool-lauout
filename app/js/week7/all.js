127ca8ae86300dc5ba04504dae414ff65210db0c
che.lin <aa10327tw@gmail.com>
2020-06-02 10:17:58 +0800

    完成第七周版型

--- app/js/week7/all.js
+++ app/js/week7/all.js
@@ -3,39 +3,39 @@
     once: true,
   });
 
-  // const top = introduceContent;
+  // 介紹的 gear 圖片
+  const gearImage = document.querySelector(
+    '.introduce__item:first-child .introduce__item__image'
+  );
+  // 介紹的 strip 圖片
+  const stripImage = document.querySelector(
+    '.introduce__item:last-child .introduce__item__image'
+  );
+  // 收集的圖片
+  const collectionImage = document.querySelector('.collection__rider');
 
-  // const imgs = document.querySelectorAll('img');
-  // const h1 = document.querySelector('h1');
-  let offset = 0;
   function scrollHandler() {
     const scrollTop = window.scrollY;
-    const scrollBottom = window.scrollY + window.innerHeight;
+    const windowsHeight = window.innerHeight;
+    const { top: gearTop } = gearImage.getBoundingClientRect();
+    const { top: stripTop } = stripImage.getBoundingClientRect();
+    const { top: collectionTop } = collectionImage.getBoundingClientRect();
 
-    const img = document.querySelector('.introduce__item__image');
-    const { top, height } = document
-      .querySelector('.introduce__item__image')
-      .getBoundingClientRect();
-    const imgMiddle = top + height / 2;
-    if (imgMiddle < scrollBottom && imgMiddle > scrollTop) {
-      if (offset > 0) offset--;
-      img.style['background-position'] = `center left calc(50% - ${offset}px)`;
-    } else {
-      if (offset <= 100) offset++;
-      console.log(offset);
-
-      img.style['background-position'] = `center left calc(50% + ${offset}px)`;
+    if (scrollTop + windowsHeight / 2 >= gearTop) {
+      gearImage.style['background-position'] = `-${parseInt(
+        scrollTop / 6
+      )}px center`;
     }
-
-    // if(introduceContentRect.top)
-    // imgs.forEach(img => {
-    //   const imgMiddle = img.offsetTop + img.height / 2;
-    //   if (imgMiddle < scrollBottom && imgMiddle > scrollTop) {
-    //     img.classList.add('active');
-    //   } else {
-    //     img.classList.remove('active');
-    //   }
-    // });
+    if (scrollTop + windowsHeight / 2 >= stripTop) {
+      stripImage.style['background-position'] = `-${parseInt(
+        scrollTop / 6
+      )}px center`;
+    }
+    if (collectionTop >= scrollTop || collectionTop <= scrollTop) {
+      collectionImage.style['background-position'] = `center -${parseInt(
+        scrollTop / 5
+      )}px`;
+    }
   }
 
   window.addEventListener('scroll', scrollHandler);
